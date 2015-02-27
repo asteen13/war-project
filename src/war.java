af6f6fe68be5b521f28cfad9946cc76fbdc67160
maliquewilliams <ohaji17@gmail.com>
2015-02-27 14:45:37 -0500

    war -> War
    im done doing changes now

--- src/war.java
+++ src/war.java
@@ -1,14 +1,12 @@
-
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
-public class war {
+public class War {
 
-	public static void main(String[ ] args) {
-	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String card;
+		
 		
 	}
 }
