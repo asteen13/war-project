3454b980337eaabd0163d483338d2aa579f1964c
asteen13 <asteen13@windstream.net>
2015-03-20 14:28:46 -0400

    started adding code

--- src/war.java
+++ src/war.java
@@ -5,8 +5,24 @@ public class War {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String card;
+		int[] deck;
+		
 		
 		
 	}
+	
+	public static void highestCard(int card1, int card2) {
+		if (card1 > card2)
+			JOptionPane.showMessageDialog(null, "You win.");
+		else if (card1 < card2)
+			JOptionPane.showMessageDialog(null, "You lose.");
+		else if (card1 == card2) {
+			int card3, card4;
+			//card3 = next card from player deck
+			//card4 = next card from computer deck
+			highestCard(card3, card4);
+		}
+			
+	}
+	
 }
