0310ccba3dae7efbb4a4562d1221f726e09aa122
maliquewilliams <ohaji17@gmail.com>
2015-03-24 16:19:32 -0400

    changes from class today
    Sadly, there's no sample program like the one in class. BUT this code
    picks random cards and displays them in the console.

--- src/war.java
+++ src/war.java
@@ -1,12 +1,19 @@
+//	Test everything using Scanner first and switch to JOptionPane when we're all done.
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
+//	Let's play a game of War!
 public class War {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String card;
 		
+		//	Introduce the whole deck.
+		String[] deck = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+		String[] suit = {"Spades", "Hearts", "Clubs", "Diamonds"};
 		
+		double card = Math.random() * deck.length ;
+		double wSuit = Math.random() * suit.length;
+		
+		System.out.print(deck[(int) card] + " of " + suit[(int)wSuit]);
 	}
 }
